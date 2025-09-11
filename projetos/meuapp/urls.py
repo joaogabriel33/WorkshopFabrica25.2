f45b1e9c2a32148b5aed7878cc863dace27f22ca
joaogabriel33 <jgliraleite@gmail.com>
2025-09-11 15:33:39 -0300

    Arquivos
    Criei alguns arquivos.

--- projetos/meuapp/urls.py
+++ projetos/meuapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import listar_pessoas, criar_pessoa, deletar_pessoa
 
 urlpatterns = [
-    path('index/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('listar/', listar_pessoas, name='listar_pessoas'),
+    path('criar/', criar_pessoa, name='criar_pessoa'),
+    path('deletar/', deletar_pessoa, name='deletar_pessoa'),
 ]
--- projetos/meuapp/views.py
+++ projetos/meuapp/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Pessoa
+from .forms import PessoaForm
 
 def home(request):
     return render(request, 'home.html')
+
+def listar_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'list.html', {'pessoas': pessoas})
+
+def criar_pessoa(request):
+    if request.method == 'POST':
+        form = PessoaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_pessoas')
+    else:
+        form = PessoaForm()
+    return render(request, 'create.html', {'pessoa': form})
+
+def deletar_pessoa(request, pk):
+    pessoa = Pessoa.objects.get(pk=pk)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('listar_pessoas')
+    return render(request, 'confirmar_delete.html', {'pessoa': pessoa})
+
+
+
+
+
